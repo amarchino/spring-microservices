869f7574743cb8398a9f77832867dd867dd8803f
amarchino <marchino.alessandro@gmail.com>
2021-07-01 23:30:05 +0200

    Lesson 25

---
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.service.BeerService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+@RequiredArgsConstructor
+public class BeerController {
+	
+	private final BeerService beerService;
+	
+	@GetMapping("/{beerId}")
+	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+		return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+	}
+
+}
---
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/service/BeerServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscbrewery.web.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+	@Override
+	public BeerDto getBeerById(UUID beerId) {
+		return BeerDto.builder()
+				.id(UUID.randomUUID())
+				.beerName("Galaxy Cat")
+				.beerStyle("Pale Ale")
+				.build();
+	}
+
+}
