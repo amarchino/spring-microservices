f184b7355ff3cd09a58b96f2738cfb84552af884
amarchino <marchino.alessandro@gmail.com>
2021-07-02 19:30:34 +0200

    Lesson 31

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class BeerController {
 	}
 
 	@PostMapping
-	public ResponseEntity<Object> handlePost(BeerDto beerDto) {
+	public ResponseEntity<Object> handlePost(@RequestBody BeerDto beerDto) {
 		BeerDto savedBeer = beerService.save(beerDto);
 		HttpHeaders headers = new HttpHeaders();
 		// TODO: add hostname to URL
