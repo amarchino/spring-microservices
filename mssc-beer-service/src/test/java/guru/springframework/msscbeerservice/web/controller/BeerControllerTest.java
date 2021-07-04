2da2fd01ec276239f7c80e55181a3e5531e4e6db
amarchino <marchino.alessandro@gmail.com>
2021-07-04 21:46:30 +0200

    Lesson 43

---
+++ mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,51 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+@WebMvcTest
+class BeerControllerTest {
+	@Autowired private MockMvc mockMvc;
+	@Autowired private ObjectMapper objectMapper;
+
+	@Test
+	void getBeerById() throws Exception {
+		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
+	@Test
+	void saveNewBeer() throws Exception {
+		BeerDto beerDto = BeerDto.builder().build();
+		
+		mockMvc.perform(
+				post("/api/v1/beer")
+				.content(objectMapper.writeValueAsString(beerDto))
+				.contentType(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(status().isCreated());
+	}
+	@Test
+	void updateBeerById() throws Exception {
+		BeerDto beerDto = BeerDto.builder().build();
+		mockMvc.perform(
+				put("/api/v1/beer/" + UUID.randomUUID())
+				.content(objectMapper.writeValueAsString(beerDto))
+				.contentType(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(status().isNoContent());
+	}
+}
