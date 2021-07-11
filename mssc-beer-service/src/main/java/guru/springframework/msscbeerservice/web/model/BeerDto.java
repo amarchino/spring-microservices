bbb4d248b591f136d37fd79e08da8853ef6278d7
amarchino <marchino.alessandro@gmail.com>
2021-07-11 13:50:15 +0200

    Lesson 69

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,16 +19,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+	@Null
 	private UUID id;
+	@Null
 	private Integer version;
 	
+	@Null
 	private OffsetDateTime createdDate;
+	@Null
 	private OffsetDateTime lastModifiedDate;
 	
-	private String name;
+	@NotBlank
+	private String beerName;
+	@NotNull
 	private BeerStyleEnum style;
+	@NotNull
+	@Positive
 	private Long upc;
 	
+	@NotNull
+	@Positive
 	private BigDecimal price;
 	private Integer quantityOnHand;
 }
