2c424a33748e1b77daff1d08975f5344b6b85789
amarchino <marchino.alessandro@gmail.com>
2021-07-18 22:15:47 +0200

    Lesson 90

--- mssc-restdocs-example/src/test/java/guru/springframework/msscrestdocsexample/web/controller/BeerControllerTest.java
+++ mssc-restdocs-example/src/test/java/guru/springframework/msscrestdocsexample/web/controller/BeerControllerTest.java
@@ -9,9 +9,12 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,6 +22,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.msscrestdocsexample.web.model.BeerDto;
 import guru.springframework.msscrestdocsexample.web.model.BeerStyleEnum;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest
 class BeerControllerTest {
 	@Autowired private MockMvc mockMvc;
