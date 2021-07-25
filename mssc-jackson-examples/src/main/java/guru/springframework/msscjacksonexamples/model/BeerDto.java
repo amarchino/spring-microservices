6f87172611eb75d7fa3ba0acb4e1f0b18da8e77c
amarchino <marchino.alessandro@gmail.com>
2021-07-25 22:00:53 +0200

    Lesson 109

--- mssc-jackson-examples/src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ mssc-jackson-examples/src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BeerDto {
+	@JsonProperty("beerId")
 	@Null
 	private UUID id;
 	@NotBlank
--- mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -18,7 +18,7 @@ class BeerDtoTest extends BaseTest {
 	
 	@Test
 	void testDeserialize() throws JsonMappingException, JsonProcessingException {
-		String json = "{\"id\":\"f7f1c925-884e-48a1-9ffd-4ab4e1eb51a4\",\"beerName\":\"Beer name\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2021-07-25T21:42:21.045528+02:00\",\"lastUpdatedTime\":\"2021-07-25T21:42:21.046552+02:00\"}";
+		String json = "{\"beerId\":\"f7f1c925-884e-48a1-9ffd-4ab4e1eb51a4\",\"beerName\":\"Beer name\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2021-07-25T21:42:21.045528+02:00\",\"lastUpdatedTime\":\"2021-07-25T21:42:21.046552+02:00\"}";
 		BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 		System.out.println(dto);
 	}
