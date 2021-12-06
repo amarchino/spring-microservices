2a28489cf96b4379f04d3d9859232ace165c045b
amarchino <marchino.alessandro@gmail.com>
2021-12-06 23:56:30 +0100

    Lesson 175

--- sfg-jms/src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HelloWorldMessage implements Serializable {
-  private static final long serialVersionUID = -1603152717742347196L;
-  private UUID id;
-  private String message;
+	private static final long serialVersionUID = -1603152717742347196L;
+	private UUID id;
+	private String message;
 }
