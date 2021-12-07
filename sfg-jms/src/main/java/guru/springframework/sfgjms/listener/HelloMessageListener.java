2ce5226ea07492469dd4d56030fcb3b3e06b5961
amarchino <marchino.alessandro@gmail.com>
2021-12-07 23:35:09 +0100

    Lesson 180

--- sfg-jms/src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgjms.listener;
 
+import java.util.UUID;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,9 +14,12 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+	private final JmsTemplate jmsTemplate;
 
 	@JmsListener(destination = JmsConfig.MY_QUEUE)
 	public void listen(
@@ -21,5 +28,18 @@ public class HelloMessageListener {
 			Message message) {
 		System.out.println("I Got a message!!!");
 		System.out.println(helloWorldMessage);
+	}
+	
+	@JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+	public void listenForHello(
+			@Payload HelloWorldMessage helloWorldMessage,
+			@Headers MessageHeaders headers,
+			Message message) throws JMSException {
+		HelloWorldMessage payloadMsg = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("World!")
+				.build();
+		jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
 	}
 }
--- sfg-jms/src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -2,9 +2,16 @@ package guru.springframework.sfgjms.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
@@ -15,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class HelloSender {
 
 	private final JmsTemplate jmsTemplate;
+	private final ObjectMapper objectMapper;
 	
 	@Scheduled(fixedRate = 2000)
 	public void sendMessage() {
@@ -27,6 +35,26 @@ public class HelloSender {
 				.build();
 		jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 		System.out.println("Message sent!");
+	}
+	
+	@Scheduled(fixedRate = 2000)
+	public void sendAndReceiveMessage() throws JMSException {
+		HelloWorldMessage message = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("Hello!")
+				.build();
+		Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, session -> {
+			try {
+				TextMessage helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+				helloMessage.setStringProperty("_type", message.getClass().getName());
+				System.out.println("Sending hello");
+				return helloMessage;
+			} catch (JsonProcessingException e) {
+				throw new JMSException("boom");
+			}
+		});
+		System.out.println(receivedMsg.getBody(String.class));
 	}
 
 }
