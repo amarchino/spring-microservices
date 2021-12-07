3f0068cf30f8869fde10aa7b45f29dac27dbd8c2
amarchino <marchino.alessandro@gmail.com>
2021-12-07 23:21:19 +0100

    Lesson 179

---
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgjms.listener;
+
+import javax.jms.Message;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+
+@Component
+public class HelloMessageListener {
+
+	@JmsListener(destination = JmsConfig.MY_QUEUE)
+	public void listen(
+			@Payload HelloWorldMessage helloWorldMessage,
+			@Headers MessageHeaders headers,
+			Message message) {
+		System.out.println("I Got a message!!!");
+		System.out.println(helloWorldMessage);
+	}
+}
