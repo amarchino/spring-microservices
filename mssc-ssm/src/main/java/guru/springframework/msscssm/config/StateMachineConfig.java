22056c713edaff62f1a931761645a0269f7e0e5e
amarchino <marchino.alessandro@gmail.com>
2021-12-09 21:41:39 +0100

    Lesson 207

--- mssc-ssm/src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -5,14 +5,19 @@ import java.util.EnumSet;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
 
 import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableStateMachineFactory
+@Slf4j
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
 
 	@Override
@@ -32,5 +37,16 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
 			.withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
 			.and()
 			.withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
+	}
+	
+	@Override
+	public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
+		StateMachineListenerAdapter<PaymentState, PaymentEvent> adapter = new StateMachineListenerAdapter<>() {
+			@Override
+			public void stateChanged(State<PaymentState, PaymentEvent> from, State<PaymentState, PaymentEvent> to) {
+				log.info(String.format("StateChanged(from: %s, to: %s)", from != null ? from.getId() : null, to != null ? to.getId() : null));
+			}
+		};
+		config.withConfiguration().listener(adapter);
 	}
 }
