4d0d66f6109de61d2a2ccde68916d8c695b70eca
amarchino <marchino.alessandro@gmail.com>
2021-12-11 18:58:52 +0100

    Lesson 212

--- mssc-ssm/src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import guru.springframework.msscssm.domain.Payment;
 import guru.springframework.msscssm.domain.PaymentEvent;
@@ -29,23 +30,26 @@ public class PaymentServiceImpl implements PaymentService {
 	}
 
 	@Override
+	@Transactional
 	public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
-		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
-		sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORIZE);
-		return sm;
-	}
-
-	@Override
-	public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
 		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_APPROVED);
 		return sm;
 	}
 
 	@Override
+	@Transactional
+	public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+		sendEvent(paymentId, sm, PaymentEvent.AUTH_APPROVED);
+		return sm;
+	}
+
+	@Override
+	@Transactional
 	public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
 		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
-		sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_DECLINED);
+		sendEvent(paymentId, sm, PaymentEvent.AUTH_DECLINED);
 		return sm;
 	}
 	
---
+++ mssc-ssm/src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscssm.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.transaction.annotation.Transactional;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+	
+	@Autowired private PaymentService paymentService;
+	@Autowired private PaymentRepository paymentRepository;
+	private Payment payment;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		payment = Payment.builder().amount(new BigDecimal("12.99")).build();
+	}
+
+	@Test
+	@Transactional
+	void preAuth() {
+		Payment savedPayment = paymentService.newPayment(payment);
+		System.out.println("Should be NEW: " + savedPayment.getState());
+		assertThat(savedPayment.getState()).isEqualTo(PaymentState.NEW);
+		StateMachine<PaymentState, PaymentEvent> stateMachine = paymentService.preAuth(savedPayment.getId());
+		Payment preAuthedPayment = paymentRepository.getById(savedPayment.getId());
+		System.out.println("Should be PRE_AUTH: " + stateMachine.getState().getId());
+		System.out.println(preAuthedPayment);
+		assertThat(preAuthedPayment.getState()).isEqualTo(PaymentState.PRE_AUTH);
+	}
+
+}
