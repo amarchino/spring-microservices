6e9542180ee56fd71bcf9f8f2917966be8cdf9da
amarchino <marchino.alessandro@gmail.com>
2021-12-11 19:46:32 +0100

    Assignment 3

---
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/config/actions/PreAuthApprovedAction.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscssm.config.actions;
+
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class PreAuthApprovedAction implements Action<PaymentState, PaymentEvent> {
+
+	@Override
+	public void execute(StateContext<PaymentState, PaymentEvent> context) {
+		log.info("PreAuthApprovedAction");
+	}
+
+}
---
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/config/guards/PaymentIdGuard.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscssm.config.guards;
+
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.guard.Guard;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.services.PaymentServiceImpl;
+
+@Component
+public class PaymentIdGuard implements Guard<PaymentState, PaymentEvent> {
+
+	@Override
+	public boolean evaluate(StateContext<PaymentState, PaymentEvent> context) {
+		return context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER) != null;
+	}
+
+}
